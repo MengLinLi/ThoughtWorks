e258617fb7d156d491cd18796ab673a37bc875f6
ZhiqiangGuo1994 <1036105855@qq.com>
2020-06-18 14:56:11 +0800

    ThoughtWorks first commit

---
+++ src/com/thoughtworks/test/FizzBuzzTests.java
@@ -0,0 +1,76 @@
+package com.thoughtworks.test;
+
+import com.thoughtworks.imp.FizzBuzz;
+import jdk.jfr.StackTrace;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class FizzBuzzTests {
+
+    /**
+     * @
+     */
+    @Test
+    public void should_return_1_given_1_when_say(){
+        /*given*/
+
+        /*when*/
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.say(1);
+
+        /*then*/
+        assertThat(result,is("1"));
+    }
+
+    @Test
+    public void shoud_return_Fizz_given_3_when_say(){
+        /*given*/
+
+        /*when*/
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.say(3);
+
+        /*then*/
+        assertThat(result,is("Fizz"));
+    }
+
+    @Test
+    public void shoud_return_Buzz_given_5_when_say(){
+        /*given*/
+
+        /*when*/
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.say(5);
+
+        /*then*/
+        assertThat(result,is("Buzz"));
+    }
+
+    @Test
+    public void shoud_return_Whizz_given_7_when_say(){
+        /*given*/
+
+        /*when*/
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.say(7);
+
+        /*then*/
+        assertThat(result,is("Whizz"));
+    }
+
+    @Test
+    public void shoud_return_FizzBuzzWhizz_given_15_when_say(){
+        /*given*/
+
+        /*when*/
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.say(15);
+
+        /*then*/
+        assertThat(result,is("FizzBuzz"));
+    }
+
+}
